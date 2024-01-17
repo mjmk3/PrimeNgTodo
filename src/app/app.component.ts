8d4ef33f6e8faa98bba43eea5b586e16f38036ee
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-17 04:03:46 +0300

    PrimeNg Todo List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CardModule} from "primeng/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
